0cb8bfd449e21d11fbaf91040da1c5ce945893a5
aily9661 <aily9661@colorado.edu>
2023-08-29 15:01:09 -0600

    initial programs

---
+++ MP/MP01 Hello_World.py
@@ -0,0 +1,3 @@
+print("Hello, world!")
+phrase = "Hello, world!"
+print(phrase)
---
+++ Program Challenge/PC01 House_Of_Dust.py
@@ -0,0 +1,11 @@
+print("Welcome to the poem generator!")
+
+material = input("What is your house made of?")
+location = input("Where is your house?")
+lightSource = input("What kind of lightsource do you use?")
+inhabitant = input("What inhabits your house?")
+
+print("A house of ", material)
+print(location)
+print("Using ", lightSource)
+print("Inhabited by ", inhabitant)
