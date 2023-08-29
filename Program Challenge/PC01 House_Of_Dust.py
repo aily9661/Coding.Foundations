74b02cbb9b418b62ccd7617f3aec0f8e41361d38
aily9661 <aily9661@colorado.edu>
2023-08-29 15:32:04 -0600

    Update PC01 House_Of_Dust.py

--- Program Challenge/PC01 House_Of_Dust.py
+++ Program Challenge/PC01 House_Of_Dust.py
@@ -1,11 +1,11 @@
 print("Welcome to the poem generator!")
 
-material = input("What is your house made of?")
-location = input("Where is your house?")
-lightSource = input("What kind of lightsource do you use?")
-inhabitant = input("What inhabits your house?")
+material = input("What is your house made of? ")
+location = input("Where is your house? ")
+lightSource = input("What kind of lightsource do you use? ")
+inhabitant = input("What inhabits your house? ")
 
-print("A house of ", material)
+print("A house of " + material)
 print(location)
-print("Using ", lightSource)
-print("Inhabited by ", inhabitant)
+print("Using " + lightSource)
+print("Inhabited by " + inhabitant)
