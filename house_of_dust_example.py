ee43fdf50551b9231b05ffaecce8d7bb18011dae
aily9661 <aily9661@colorado.edu>
2023-09-01 17:14:43 -0600

    a

---
+++ house_of_dust_example.py
@@ -0,0 +1,14 @@
+greeting = "\nHello, welcome to the poem generator!"
+print(greeting)
+print("\n\n")
+
+material = input("What is your house made of? ")
+location = input("Where is your house? ")
+lights = input("How do you light up your house?")
+inhabit = input("What inhabits your house?")
+print("\n\n")
+print("A house of ",material)
+print("     ",location)
+print("         Using", lights)
+print("            Inhabited by",inhabit)
+print("\n\n")
