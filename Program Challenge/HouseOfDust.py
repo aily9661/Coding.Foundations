95d379f08d432e2957e3bd638b36e7f720a96a7e
aily9661 <aily9661@colorado.edu>
2023-09-01 09:57:15 -0600

    replace house of dust

---
+++ Program Challenge/HouseOfDust.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python
+
+#print an introductory message
+print("\n\nLet's write a poem together!\n\n")
+
+#make 4 variables, one for material of your house
+#one for the location of your house
+#one for how it is illumnated and one
+#one for who it is inhabited by
+
+material = input("What is your house made of? ")
+location = input("Where is your house? ")
+lighting = input("How is your house illuminated? ")
+inhabitants = input("Who or what inhabits your house? ")
+
+#print out the "House of Dust" poem
+#
+print("\n\n") #print 2 blank lines
+print("A House of",material)
+print("    ",location)
+print("        Using",lighting)
+print("            Inhabited by",inhabitants)
+print("\n\n") #print 2 blank lines
